ee8198ab6404b7c07b3832b425433aa2f665b0d5
mli254 <mli254@ucsc.edu>
2023-06-02 23:56:54 -0700

    updating initial files

---
+++ game.js
@@ -0,0 +1,41 @@
+class FirstLoad extends Phaser.Scene {
+    constructor() {
+        super('firstload');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+}
+
+class SecondLoad extends Phaser.Scene {
+    constructor() {
+        super('secondload');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        
+    }
+}
+
+class ThirdLoad extends Phaser.Scene {
+    constructor() {
+        super('thirdload');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        
+    }
+}
